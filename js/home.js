1c78c56d54715776c439e22bbe7f434230d00dd2
Benoit <benoitoehmichen@outlook.fr>
2025-01-29 10:52:04 +0100

    finish display search result

--- js/home.js
+++ js/home.js
@@ -1,35 +1,38 @@
-console.log("Hello");
-
 const searchTrips = () => {
   const departureEntered = document.querySelector("#departure").value;
-  console.log("departureEntered = ", departureEntered);
 
   const arrivalEntered = document.querySelector("#arrival").value;
-  console.log("arrivalEntered = ", arrivalEntered);
 
   const dateEntered = document.querySelector("#date").value;
-  console.log("dateEntered = ", dateEntered);
 
-  fetch("http://localhost:3000/home/trips")
+  const url = `http://localhost:3000/home/trips?departure=${departureEntered}&arrival=${arrivalEntered}&date=${dateEntered}`;
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
     .then((response) => response.json())
     .then((tripRecieved) => {
       console.log("tripRecieved = ", tripRecieved);
-    });
-
-  const searchedData = {
-    departure: departureEntered,
-    arrival: arrivalEntered,
-    date: dateEntered,
-  };
-
-  fetch("https://api.chucknorris.io/jokes/random", {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(searchedData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("data = ", data);
+      if (!tripRecieved.result) {
+        document.querySelector("#results").innerHTML = `
+               <img class="rightIcon" src="./images/notfound.png" />
+            <div id="greenSeparator"></div>
+            <h5>No trip found.</h5>
+            `;
+      } else {
+        document.querySelector("#results").innerHTML = ``;
+        for (let oneTrip of tripRecieved.trips) {
+          document.querySelector("#results").innerHTML += `
+               <div class="oneResult">
+            <p>${oneTrip.departure} > ${oneTrip.arrival}</p>
+            <p>${oneTrip.time}</p>
+            <p>${oneTrip.price}€</p>
+            <button class="bookButton" id=${oneTrip.id}>Book</button>
+          </div>`;
+        }
+      }
     });
 };
 
